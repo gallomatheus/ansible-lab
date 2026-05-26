90e5a715eceb10161ba1592dca5012e26e126dd9
Matheus Gallo <gamatheus12@outlook.com>
2026-05-26 20:29:34 -0300

    Atualizando repo

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -1,9 +1,23 @@
-#/bin/sh
-sudo yum -y install epel-release
-echo "inicio da instalacao do ansible"
-sudo yum -y install ansible
-cat <<EOT >> /etc/hosts
-192.168.1.2 control-node
-192.168.1.3 app01
-192.168.1.4 db01
-EOT
+#!/bin/bash
+set -e
+
+apt-get update
+apt-get install -y \
+ansible \
+sshpass \
+git \
+curl \
+vim
+
+echo "Control-node configurado!"
+
+
+# #/bin/sh
+# sudo yum -y install epel-release
+# echo "inicio da instalacao do ansible"
+# sudo yum -y install ansible
+# cat <<EOT >> /etc/hosts
+# 192.168.1.2 control-node
+# 192.168.1.3 app01
+# 192.168.1.4 db01
+# EOT
